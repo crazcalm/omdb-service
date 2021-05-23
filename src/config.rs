6b729dfe57d508701af08446d2631567348b0434
Marcus Willock <crazcalm@gmail.com>
2021-05-22 23:19:21 -0400

    Add config setup

---
+++ src/config.rs
@@ -0,0 +1,20 @@
+use dotenv;
+use std::path::Path;
+
+pub async fn load_config(env: String) -> Result<std::path::PathBuf, dotenv::Error> {
+    let config_path = Path::new("config").join(env);
+    dotenv::from_filename(config_path)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config;
+    use std::env;
+
+    #[tokio::test]
+    async fn test_load_config() {
+        config::load_config("test".to_string()).await.unwrap();
+
+        assert_eq!("test_name", env::var("NAME").unwrap());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+mod config;
 mod handlers;
 mod models;
 mod routes;
 
+use std::env;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // Ensure that an environment is setup
+    if env::var("ENV").is_err() {
+        env::set_var("ENV", "local");
+    };
 
+    // Get the enviroment variable
+    let environment = env::var("ENV").expect("Failed to get the ENV variable");
+
+    config::load_config(environment).await.unwrap();
+
+    // Setup the routes
     let api = routes::health::register();
 
     warp::serve(api).run(([127, 0, 0, 1], 3030)).await;
